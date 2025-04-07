575a459675802b00a3a81249f49341331143d87f
PoiLson <milonakidanai@gmail.com>
2025-04-07 21:48:44 +0300

    Adding some final touches and some test cases!

--- Part2/Main.java
+++ Part2/Main.java
@@ -1,9 +1,26 @@
 import java_cup.runtime.*;
 import java.io.*;
 
-class Main {
-    public static void main(String[] argv) throws Exception{
-        Parser p = new Parser(new Scanner(new InputStreamReader(System.in)));
-        p.parse();
+class Main
+{
+    public static void main(String[] argv) throws Exception
+    {
+        // Implementing the first stage of our compiler
+        // input -> IR language!
+       
+        PrintStream outputTxt = new PrintStream(new FileOutputStream("Translated.ir"));
+        System.setOut(outputTxt);
+
+        Parser pIR = new Parser(new Scanner(new InputStreamReader(System.in)));
+        pIR.parse();
+
+        
+        // Implementing the second stage of our compiler
+        // IR -> Java code!
+        outputTxt = new PrintStream(new FileOutputStream("Translated.java"));
+        System.setOut(outputTxt);
+
+        ParserJava pJava = new ParserJava(new Scanner(new FileReader("Translated.ir")));
+        pJava.parse();
     }
 }
